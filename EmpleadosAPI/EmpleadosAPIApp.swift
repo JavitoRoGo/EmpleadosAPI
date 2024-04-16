c32ce8345a1603bcefea867b264e35ac2d85f85d
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2024-04-16 18:42:50 +0100

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Javier Rodríguez Gómez on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
